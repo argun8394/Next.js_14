2404d7d69334e319ccbe928b03f26396edecca48
argun8394 <argundoganali@gmail.com>
2024-02-29 01:06:44 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Argun</div>
+      <div className={styles.text}>
+        Argun creative thoughts agency © All Rights Reserved
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
