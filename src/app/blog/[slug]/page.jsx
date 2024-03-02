296cae04353921eaae5d9308aab13e0efedd72e4
argun8394 <argundoganali@gmail.com>
2024-03-03 00:14:01 +0300

    post

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/post.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="/post.png"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>John Doe</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.03.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In assumenda
+          iure et, reiciendis saepe deleniti, earum ex asperiores dolores sit
+          perspiciatis. Quis rerum, doloremque accusamus doloribus modi sequi
+          alias iste?
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
