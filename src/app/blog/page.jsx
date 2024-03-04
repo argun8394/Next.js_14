c439c8ec80f611d176cd0374f49e650340ffb9b1
argun8394 <argundoganali@gmail.com>
2024-03-04 23:53:45 +0300

    blog Page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,8 +2,12 @@ import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 
 const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "force-cache",
+  });
+  //default {cache: "force-cache"}
+  //no cache  {cache: "no-store"}
+  // {next:{revalidate:3600}}
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
