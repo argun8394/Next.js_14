19a3962e0a03ead572fca068b97ab9ed46d089c0
argun8394 <argundoganali@gmail.com>
2024-03-04 23:45:42 +0300

    postCard

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -2,23 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./postCard.module.css";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/post.png" alt="" fill className={styles.img} />
+          <Image src="/post.jpg" alt="" fill className={styles.img} />
         </div>
         <span className={styles.date}>01.03.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Exercitationem sint magni dolor ratione reiciendis voluptas est autem.
-          Alias officia, voluptate sint soluta nulla quisquam veritatis
-          expedita, optio adipisci, possimus laudantium.
-        </p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
         <Link className={styles.link} href="/blog/post">
           READ MORE
         </Link>
