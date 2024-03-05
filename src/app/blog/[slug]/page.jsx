022a54749e43481fc47f9dce9b40f1e709bf2822
argun8394 <argundoganali@gmail.com>
2024-03-05 23:42:22 +0300

    single_post

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,14 +1,34 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${slug}`,
+    {
+      cache: "force-cache",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/post.jpg" alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -17,21 +37,13 @@ const SinglePostPage = () => {
             width={50}
             height={50}
           />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>John Doe</span>
-          </div>
+          <PostUser userId={post.userId} />
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>01.03.2024</span>
           </div>
         </div>
-        <div className={styles.content}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In assumenda
-          iure et, reiciendis saepe deleniti, earum ex asperiores dolores sit
-          perspiciatis. Quis rerum, doloremque accusamus doloribus modi sequi
-          alias iste?
-        </div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
